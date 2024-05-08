26ba834d1347c98358db719b4d5eb910dc2c6df6
Ajay Kaloty <kalotya99@gmail.com>
2024-05-07 22:53:29 -0700

    Added changes to the server.js

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
+// Import required modules
 const express = require("express");
+// Import database connection
 const db = require("./config/connection");
+// Import routes
 const routes = require('./routes');
 
+//Set port
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-
+// Middleware for parsing JSON and urlencoded data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+//Use routes
 app.use(routes);
 
+//Database connection eventy listener 
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
