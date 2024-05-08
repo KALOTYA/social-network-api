22056a7334701d8a7fd9d2bfd8b428554030038d
Ajay Kaloty <kalotya99@gmail.com>
2024-05-07 22:48:43 -0700

    Added changes to the user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,6 +3,7 @@ const Thought = require("../models/Thought");
 const { ObjectId } = require("mongodb");
 
 module.exports = {
+  //Get all users
   async getUsers(req, res) {
     try {
       const users = await User.find();
@@ -11,6 +12,7 @@ module.exports = {
       res.status(500).json(err);
     }
   },
+  // Get a single user by ID
   async getSingleUser(req, res) {
     try {
       const objId = new ObjectId(req.params.userId);
@@ -20,6 +22,7 @@ module.exports = {
       res.status(500).json(err);
     }
   },
+  // Update a user
   async updateUser(req, res) {
     try {
       const updateuser = await User.findOneAndUpdate(
@@ -34,7 +37,7 @@ module.exports = {
       console.log(err);
     }
   },
- 
+ // Create a new user
   async createUser(req, res) {
     try {
       const dbUserData = await User.create(req.body);
@@ -43,11 +46,13 @@ module.exports = {
       res.status(500).json(err);
     }
   },
+  // Delete a user
   async deleteUser(req, res) {
     try {
       const objId = new ObjectId(req.params.userId);
-
+      // Find and delete the user by ID
       const deleteuser = await User.findOneAndDelete({ _id: objId });
+      // Delete all thoughts associated with the deleted user
       
       await Thought.deleteMany({ _id: { $in: deleteuser.thoughts } });
       res.json(deleteuser);
@@ -55,10 +60,12 @@ module.exports = {
       res.status(500).json(err);
     }
   },
+  // Add a friend to user's friend list
   async addFriend(req, res) {
     try {
       const userId = req.params.userId;
       const friendId = req.params.friendId;
+       // Find user by ID and add friend to friends array
       const addfriend = await User.findOneAndUpdate(
         { _id: userId },
         { $addToSet: { friends: friendId } },
@@ -69,10 +76,12 @@ module.exports = {
       res.status(500).json(err);
     }
   },
+  // Remove a friend from user's friend list
   async removeFriend(req, res) {
     try {
       const userId = req.params.userId;
       const friendId = req.params.friendId;
+       // Find user by friend ID and remove friend from friends array
       const removefriend = await User.findOneAndUpdate(
         { friends: friendId },
         { $pull: { friends: friendId } },
